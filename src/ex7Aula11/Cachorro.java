8747ca4b48bee8abdb67d0069fb9c0d97acd946c
NandaMariaa <nandamari182@gmail.com>
2025-04-24 21:18:37 -0300

    initial commit

--- src/ex7Aula11/Cachorro.java
+++ src/ex7Aula11/Cachorro.java
@@ -1,7 +1,37 @@
 public class Cachorro {
 
+    private RacasDeCachorro raca;
     private String nome;
-    private RacasDeCachorro raça;
-    private int idade;
+    private byte idade;
+ 
+    public RacasDeCachorro getRaca() {
+        return raca;
+    }
+    public void setRaca(RacasDeCachorro raca) {
+        this.raca = raca;
+    }
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public byte getIdade() {
+        return idade;
+    }
+    public void setIdade(byte idade) {
+        this.idade = idade;
+    }
+ 
+    public Cachorro(RacasDeCachorro raca, String nome, byte idade) {
+        this.raca = raca;
+        this.nome = nome;
+        this.idade = idade;
+    }
+ 
+    @Override
+    public String toString() {
+        return "Cachorro [raca=" + raca + ", nome=" + nome + ", idade=" + idade + "]";
+    }
     
 }
--- src/ex7Aula11/Teste.java
+++ src/ex7Aula11/Teste.java
@@ -1,3 +1,16 @@
+import java.util.ArrayList;
+ 
 public class Teste {
-    
+    public static void main(String args[]) {
+        var listaDeCachorros = new ArrayList<Cachorro>();
+ 
+        for(int i = 0; i < listaDeCachorros.size(); i++) {
+            listaDeCachorros.add(new Cachorro(RacasDeCachorro.VIRA_LATA, String.valueOf(Math.random()), (byte)(Math.random() * 20)));
+        }
+ 
+        System.out.println("#####");
+        for(Cachorro c : listaDeCachorros) {
+            System.err.println(c);
+        }
+    }
 }
